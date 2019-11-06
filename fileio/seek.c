a6d22f8dfa1285d2646e267ca592d96e58364a2d
MemoryReload <heping_tsdwx@163.com>
2019-11-07 00:14:19 +0800

    more things

--- fileio/seek.c
+++ fileio/seek.c
@@ -1,9 +1,11 @@
 #import "apue.h"
+#include <errno.h>
 
 int main (){
     if (lseek(STDIN_FILENO,0,SEEK_CUR) == -1){
         //If the file descriptor refers to a pipe, FIFO, or socket, lseek sets errno to ESPIPE and returns −1
-        printf("cannot seek!\n");
+        printf("Error:%d",errno);
+        perror(__FILE__);
     } else{
         printf("seek OK!\n");
     }
--- intro/testerror.c
+++ intro/testerror.c
@@ -1,4 +1,11 @@
-//
-// Created by HePing on 2019/11/2.
-//
+#include "apue.h"
+#include <errno.h>
 
+int main (int argc, char *argv[]){
+    //打印到标准错误流，strerror把错误码转换成描述
+    fprintf(stderr,"EACCES: %s\n",strerror(EACCES));
+    errno = ENOENT;
+    //打印出错原因，参数为前导字符串。
+    perror(argv[0]);
+    exit(0);
+}
