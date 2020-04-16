36a2ad61b4f6dbe09c67b25a3bc024512c02a6eb
ping.he <ping.he@kuwo.cn>
2020-04-16 20:09:15 +0800

    add access.c file

---
+++ filedir/access.c
@@ -0,0 +1,25 @@
+//
+// Created by 何平 on 2020/4/16.
+//
+
+#include "apue.h"
+#include <fcntl.h>
+
+int main(int argc, char * argv[])
+{
+    if (argc != 2) {
+        err_quit("usage: a.out <pathname>");
+    }
+    if (access(argv[1],F_OK)<0)
+    {
+        err_ret("access error for %s", argv[1]);
+    } else{
+        printf("read access OK\n");
+    }
+    if (open(argv[1],O_RDONLY)<0){
+        err_ret("open error for %s", argv[1]);
+    } else{
+        printf("open for reading OK\n");
+    }
+    return 0;
+}
